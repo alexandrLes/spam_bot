6825e95e4d25a88a1a0308903881ea0e20a36204
Alexandr <63264710+alexandrLes@users.noreply.github.com>
2024-12-02 11:17:16 +0000

    Create Bot

---
+++ bot.py
@@ -0,0 +1,57 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, F, Router
+from aiogram.types import Message
+from config import SPAM_KEYWORDS, is_spam
+
+# Логирование
+logging.basicConfig(level=logging.INFO)
+
+# Инициализация бота и диспетчера
+TOKEN = "7938100044:AAEkskIwInsETF6v5dSXyZJ6OV1aZNen9ng"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+router = Router()
+
+dp.include_router(router)
+
+@router.message(F.text == "chat_id")
+async def handle_chat_id(message: Message):
+    chat_id = message.chat.id
+    await message.reply(f"ID этого чата: {chat_id}")
+
+# Проверка сообщений на наличие спам-ключевых слов
+@dp.message(F.text)
+async def handle_message(message: Message):
+    if any(keyword in message.text.lower() for keyword in SPAM_KEYWORDS) or is_spam(message.text.lower()):
+        try:
+            # Удаление сообщения
+            await message.delete()
+            
+            # Блокировка пользователя
+            await bot.ban_chat_member(chat_id=message.chat.id, user_id=message.from_user.id)
+            
+            # Уведомление администратора
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=f"Пользователь {message.from_user.full_name} ({message.from_user.id}) был заблокирован за спам."
+            )
+        except Exception as e:
+            logging.error(f"Ошибка при удалении сообщения или блокировке пользователя: {e}")
+
+    if len(message.text) > 1000:
+        try:
+            await message.delete()
+
+            await bot.send_message(
+                chat_id=message.chat.id,
+                text=f"Пользователь {message.from_user.full_name} ({message.from_user.id}) был заблокирован за спам."
+            )
+        except Exception as e:
+            logging.error(f"Ошибка при удалении сообщения или блокировке пользователя: {e}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+
+SPAM_KEYWORDS = [
+    "заработок",
+    "пассивный доход",
+    "криптовалюта",
+    "быстрые деньги",
+    "инвестиции",
+    "доход без вложений",
+    "привлекаю",
+    "заработать"
+]
+
+SPAM_PATTERNS = [
+    r"заработать.*без вложений",  # "заработать" и вариации
+    r"пассивный доход",           # "пассивный доход"
+    r"инвестиции.*биткоин",       # "инвестиции" и "биткоин"
+    r"криптовалюта",              # "криптовалюта"
+    r"легкие деньги",             # "легкие деньги"
+    r"привлекаю",
+    r"заработать",
+]
+
+def is_spam(message_text):
+    for pattern in SPAM_PATTERNS:
+        if re.search(pattern, message_text.lower()):
+            return True
+    return False
