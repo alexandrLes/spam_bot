56b3e39b2604874049b1a295fbe40d12ed192661
AlexandrLes <sashales94@mail.ru>
2025-01-08 15:45:44 +0300

    update

--- bot.py
+++ bot.py
@@ -15,6 +15,29 @@ router = Router()
 
 dp.include_router(router)
 
+def replace_hidden_chars(text):
+    replacements = {
+        'C': 'С', 'H': 'Н', 'O': 'О',
+        'u': 'и', '3': 'з', '6': 'б',
+        'Y': 'У', 'd': 'д', 'e': 'е',
+        'n': 'н', 'c': 'с', 'p': 'р',
+        'a': 'а', 'b': 'в', 'o': 'о',
+        't': 'т', 'k': 'к', 'm': 'м',
+        'x': 'х', 'r': 'р', 'q': 'к',
+        's': 'с', 'l': 'л', 'j': 'й',
+        'v': 'в', 'i': 'и', 'g': 'г',
+        'T': 'Т', 'A': 'А', 'B': 'Б',
+        'E': 'Е', 'K': 'К', 'M': 'М',
+        'X': 'Х', 'Z': 'З', '4': 'ч', 'α': 'а', 'ρ':'р', 'U':'и'
+    }
+
+    for latin_char, russian_char in replacements.items():
+        text = text.replace(latin_char, russian_char)
+
+    text = text.replace(' ', '')
+
+    return text
+
 @router.message(F.text == "chat_id")
 async def handle_chat_id(message: Message):
     chat_id = message.chat.id
@@ -22,7 +45,8 @@ async def handle_chat_id(message: Message):
 
 @dp.message(F.text)
 async def handle_message(message: Message):
-    if any(keyword in message.text.lower() for keyword in SPAM_KEYWORDS) or is_spam(message.text.lower()):
+    message_text = replace_hidden_chars(message.text)
+    if any(keyword in message.text.lower() for keyword in SPAM_KEYWORDS) or is_spam(message.text.lower()) or any(keyword in message_text.lower() for keyword in SPAM_KEYWORDS):
         try:
             # Попытка отправить сообщение в первый чат
             await bot.send_message(471761840, f"Delete message:\n\n {message.text}")
